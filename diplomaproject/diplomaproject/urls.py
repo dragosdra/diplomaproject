2a311745f2393281c2c06c5cc039dcd28b5738f3
dragosdra <137883299+dragosdra@users.noreply.github.com>
2024-02-13 21:43:11 +0200

    Add files via upload

---
+++ diplomaproject/diplomaproject/urls.py
@@ -0,0 +1,40 @@
+"""
+URL configuration for diplomaproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from polls import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('products', views.products , name="products"),
+    path('darkroom', views.darkroom, name="darkroom"),
+    path('login', views.login, name="login" ),
+    path('register', views.register, name="register"),
+    path('contact', views.contact, name="contact"),
+    path('canonae1program', views.canonae1program, name="canonae1program"),
+    path('contaxt2', views.contaxt2, name="contaxt2"),
+    path('ilfordhp5', views.ilfordhp5, name="ilfordhp5"),
+    path('ilfordxp2', views.ilfordxp2, name="ilfordxp2"),
+    path('vision350d', views.kodak50d, name="vision350d"),
+    path('vision3250d', views.kodak250d, name="vision3250d"),
+    path('vision3200t', views.kodak200t, name="vision3200t"),
+    path('vision3500t', views.kodak500t, name="vision3500t"),
+    path('kodakgold200', views.kodakgold200, name="kodakgold200"),
+    path('mamiya7', views.mamiya7, name="mamiya7"),
+    path('pentax6x7', views.pentax6x7, name="pentax6x7"),
+    path('xpan', views.xpan, name="xpan"),
+    ]
---
+++ diplomaproject/polls/views.py
@@ -0,0 +1,81 @@
+from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+def home(request):
+    return render(request, "index.html")
+
+
+def products(request):
+    return render(request, 'products.html')
+
+
+
+def darkroom(request):
+    return render(request, 'darkroom.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        user = authenticate(username= request.POST.get('username'), password= request.POST.get('password'))
+        print(user)
+    return render(request, 'login.html')
+
+
+def register(request):
+    if request.method == "POST":
+        print(request.POST)
+        from django.contrib.auth.models import User
+        User.objects.create_user(request.POST.get('username'),request.POST.get('email'),request.POST.get('password'))
+    return render(request, 'register.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def canonae1program(request):
+    return render(request, 'canonae1program.html')
+
+
+def contaxt2(request):
+    return render(request, 'contaxt2.html')
+
+
+def ilfordhp5(request):
+    return render(request, 'ilfordhp5.html')
+
+
+def ilfordxp2(request):
+    return render(request, 'ilfordxp2.html')
+
+
+def kodak50d(request):
+    return render(request, 'kodak50d.html')
+
+
+def kodak250d(request):
+    return render(request, 'kodak250d.html')
+
+
+def kodak250d(request):
+    return render(request, 'kodak250d.html')
+
+
+def kodak200t(request):
+    return render(request, 'kodak200t.html')
+
+def kodak500t(request):
+    return render(request, 'kodak500t.html')
+
+def kodakgold200(request):
+    return render(request, 'kodakgold200.html')
+
+def mamiya7(request):
+    return render(request, 'mamiya7.html')
+
+def pentax6x7(request):
+    return render(request, 'pentax6x7.html')
+
+def xpan(request):
+    return render(request, 'xpan.html')
+
+
+# Create your views here.
